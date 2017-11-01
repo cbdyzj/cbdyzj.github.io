1d8d7116aafbb7899c5969a51d76ba9ed4375ed8
cbdyzj <cbdyzj@gmail.com>
2017-11-01 20:39:49 +0800

    add http proxy to code

---
+++ notes/code/proxy.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const httpProxy = require('http-proxy')
+
+// 新建http代理
+const proxy = httpProxy.createProxyServer({ changeOrigin: true })
+
+// 配置网关
+const gateway = (req, res) => proxy.web(req, res, { target: 'http://jianzhao.org/' })
+
+// 启动http服务
+http.createServer(gateway).listen(8000, () => console.info('gateway inited to 8000'))
