4a721c808d5a5da7de2fe28107298bbd7520782c
cbdyzj <cbdyzj@gmail.com>
2017-11-03 17:55:51 +0800

    amend

--- notes/code/query.js
+++ notes/code/query.js
@@ -4,14 +4,14 @@ const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: 'rootpassword',
-    database: 'sys'
+    database: 'sys',
 })
+
 connection.connect();
 const query = (...args) =>
     new Promise((resolve, reject) =>
         connection.query(...args, (error, results, fields) =>
             error ? reject(error) : resolve({ results, fields })))
-
 
 async function q() {
     const { results } = await query('SELECT ? FROM host_summary', ['host'])
