feb84f5c2ba02a6db32bbc0438e10cab09281d57
cbdyzj <cbdyzj@gmail.com>
2017-11-03 17:51:17 +0800

    notes/code/query.js

---
+++ notes/code/query.js
@@ -0,0 +1,22 @@
+'use strict'
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'rootpassword',
+    database: 'sys'
+})
+connection.connect();
+const query = (...args) =>
+    new Promise((resolve, reject) =>
+        connection.query(...args, (error, results, fields) =>
+            error ? reject(error) : resolve({ results, fields })))
+
+
+async function q() {
+    const { results } = await query('SELECT ? FROM host_summary', ['host'])
+    console.log(results)
+    connection.end()
+}
+
+q.call()
