619f9dbedd5f39a46c732bff38f22886368bf9a6
Cbdy <cbdyzj@jianzhao.org>
2018-01-08 16:04:02 +0800

    Update sync_git.py

--- code/sync_git.py
+++ code/sync_git.py
@@ -3,7 +3,7 @@ from subprocess import run
 from time import  strftime, localtime
 
 t = strftime("%Y-%m-%d %I:%M %p",localtime())
-run(["git","add","."])
+run(["git","add","-A"])
 run(["git","commit","-m",t])
 run(["git","push"])
 print("\n" + t + "：如果没有报错的话，就同步好了：）")
